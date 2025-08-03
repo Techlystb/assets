6fe3406b26da4bfb9159d4a6ab1c3cec180f19ca
Techlystb <techlystb@gmail.com>
2025-08-04 01:20:37 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+const scripts = [
+  "https://cdn.jsdelivr.net/gh/Techlystb/assets/js/ThemeChangerAuto.min.js",
+  "https://cdn.jsdelivr.net/gh/Techlystb/assets/js/file2.js",
+  "https://cdn.jsdelivr.net/gh/Techlystb/assets/js/file3.js"
+];
+
+scripts.forEach(src => {
+  const s = document.createElement("script");
+  s.src = src;
+  document.head.appendChild(s);
+});
 
